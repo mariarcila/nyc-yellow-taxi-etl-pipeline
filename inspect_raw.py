2625498d094122c8fcafa6665cd2f1cd4679715a
Camila Arcila <maricamiarci12@gmail.com>
2025-04-19 16:22:01 -0500

    Update trusted cleaning script and add observability report

---
+++ inspect_raw.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+import os
+
+def start_spark(app_name: str = "InspectRaw"):
+    return SparkSession.builder.appName(app_name).getOrCreate()
+
+def get_parquet_paths(folder_path: str, year: str):
+    files = [
+        os.path.join(folder_path, f)
+        for f in os.listdir(folder_path)
+        if f.startswith(f"yellow_tripdata_{year}-") and f.endswith(".parquet")
+    ]
+    return files
+
+def main():
+    spark = start_spark()
+    folder_path = "data"
+    year = "2024"
+
+    parquet_files = get_parquet_paths(folder_path, year)
+    df = spark.read.parquet(*parquet_files)
+
+    df.printSchema()
+    df.select("*").limit(10).show(truncate=False)
+
+if __name__ == "__main__":
+    main()
